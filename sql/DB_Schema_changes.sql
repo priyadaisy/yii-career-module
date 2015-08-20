804638539d880c17be3f5b9914d7472c2eb746dd
nirmanyu <nirmanyu03@gmail.com>
2015-08-20 18:25:14 +0530

    add created_on in jobdescription and url_resume to job application table

--- sql/DB_Schema_changes.sql
+++ sql/DB_Schema_changes.sql
@@ -1,2 +1,8 @@
--------------------change the type of update on-------------------
+-------------------change the type of update on---------ON : 19 Aug 2015-----
 ALTER TABLE `job_description` CHANGE `update_on` `update_on` DATETIME NOT NULL ;
+
+-----add created_on field in the Job Description Table ----- ON : 20 Aug 2015------
+ALTER TABLE `job_description` ADD `created_on` DATETIME NOT NULL AFTER `status`;
+
+-----add url_resume field in the Job Application table ------  ON : 20 Aug 2015------
+ALTER TABLE `job_application` ADD `url_resume` TEXT NOT NULL AFTER `mobile`;
