c05f3aae248274948eb47e068b2c3be9e97d7f40
Napoleon Arouldas <meetneps@gmail.com>
2016-01-07 01:18:59 +0530

    Added dir (runtime,asset) creation at setup

--- index.php
+++ index.php
@@ -9,5 +9,15 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+// Create Asset and runtime folder if not created
+if (!file_exists('protected/runtime')) {
+    mkdir('protected/runtime',0777);
+}
+
+if (!file_exists('assets')) {
+    mkdir('assets',0777);
+}
+
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
