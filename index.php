87ed6620fa2a0a2fda80317279fdc00118641308
Priya Rajagopal <priyadaisy@users.noreply.github.com>
2017-12-11 13:34:24 +0530

    $yii=dirname(__FILE__).'/framework/yii.php'

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 //adding comments
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
+
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
