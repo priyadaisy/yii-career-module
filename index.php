6e6e27facbf665f94443da583df6dc8e728ae8be
Priya Rajagopal <priyadaisy@users.noreply.github.com>
2017-12-12 11:48:02 +0530

    new commit changes

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $yii=dirname(__FILE__).'/framework/yii.php';
 
 $config=dirname(__FILE__).'/protected/config/main.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
